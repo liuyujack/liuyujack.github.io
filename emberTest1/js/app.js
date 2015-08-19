7b9fb4c6ac170744ccddf6da4dcba93e9608b83d
Yu Liu <yu.liu@ucl.ac.uk>
2015-08-19 12:56:59 +0100

    Stage one learning.

--- emberTest1/js/app.js
+++ emberTest1/js/app.js
@@ -2,10 +2,26 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.route('about');
+  this.route('testimonial');
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return ['red', 'yellow', 'blue','orange','purple'];
   }
+});
+
+App.AboutRoute = Ember.Route.extend({
+  
+});
+
+App.TestimonialRoute = Ember.Route.extend({
+
+});
+
+App.IndexController = Ember.ArrayController.extend({
+	sortedModel: function() {
+		return this.get('model').sort();
+	}.property('@each')
 });
